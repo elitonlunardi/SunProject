27dfe85187731d603f6f997ce6a0724dc3736619
Eliton Lunardi <lunardi.eliton@gmail.com>
2019-05-25 21:38:20 -0300

    correção gitignore
    correção gitignore

--- Back-end/Sun/Sun.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Sun.Application
-{
-    public class Class1
-    {
-    }
-}
--- Back-end/Sun/Sun.Infra.CrossCutting.IoC/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Sun.Infra.CrossCutting.IoC
-{
-    public class Class1
-    {
-    }
-}
